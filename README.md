f994a496e37ef498d7ebd6ede248d9f882fc9901
licav <licav@hismith.ru>
2022-12-20 13:56:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Чтобы развернуть проект необходимо:
+
+$ git clone https://github.com/forw1k/overlay.git
+
+$ cd overlay
+
+$ npm install
+
+Для запуска проекта:
+
+$ npm run start
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './index.scss';
+import data from './users.json';
+
+function getData(data) {
+  setTimeout(() => {
+    listRender(data);
+  }, 10);
+}
+
+const tableBody = document.getElementById('tbody');
+
+const listRender = (data) => {
+  data.forEach((item) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td>${item.id}</td>
+      <td>${item.first_name}</td>
+      <td>${item.last_name}</td>
+      <td>${item.email}</td>
+      <td class="cell-img">
+        <img src=${item.avatar}>
+      </td>
+    `;
+    tableBody.appendChild(tr);
+  });
+};
+
+getData(data);
